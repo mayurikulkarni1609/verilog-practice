59167e5a2e2f7bc20bb32244dbf7089f4221e210
mayurikulkarni1609 <mayurikulkarni1609@gmail.com>
2026-03-31 23:35:35 +0530

    Update demux_parameter.v

--- modules/demux_parameter.v
+++ modules/demux_parameter.v
@@ -4,7 +4,7 @@ module demux1_2powerm #(parameter m=3)
     input [m-1:0] s,
     output reg [(1<<m)-1:0] y
 );
-//Parameterized 1-to-(2^m) demultiplexer
+//Parameterized 1-to-(2^m) demultiplexers
 always@(*)
   begin
     y = 0;
