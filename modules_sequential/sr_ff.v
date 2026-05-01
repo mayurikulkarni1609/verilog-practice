2bfe7f63ff74421f1ab186e9d930c1c87d8ef545
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-01 22:25:34 +0530

    SR FF module

---
+++ modules_sequential/sr_ff.v
@@ -0,0 +1,16 @@
+module sr_ff
+(
+    input clk, s, r,
+    output reg q
+);
+  always@(posedge clk)
+     begin
+        case({s,r})
+           2'b00: q <= q;
+           2'b01: q <= 0;
+           2'b10: q <= 1;
+           2'b11: q <= 2'bx;
+        endcase
+     end
+endmodule
+        
