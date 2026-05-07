5f3894f3a0641d4350029890115f63d9a3d902e8
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-07 23:43:45 +0530

    SISO shift register module

---
+++ modules_sequential/siso.v
@@ -0,0 +1,15 @@
+module siso
+(
+  input clk, reset,
+  input  [3:0]data,
+  output reg [3:0]q
+);
+always@(posedge clk or posedge reset)
+   begin
+     if(reset)
+       q <= 0;
+        else
+          begin
+            for(i=0; i<4; i=i+1)
+               begin
+                 q[]
