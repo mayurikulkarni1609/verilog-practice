48f45a5ea07990578b7b7b2eedf7e91960cd84aa
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-13 23:55:10 +0530

    verilog module for serial-in serial-out shift register

--- modules_sequential/siso.v
+++ modules_sequential/siso.v
@@ -1,15 +1,19 @@
-module siso
+module siso (#parameter n = 4)
 (
   input clk, reset,
-  input  [3:0]data,
-  output reg [3:0]q
+  input  serial_in,
+  output reg [n-1:0]q,
+  output serial_out
 );
 always@(posedge clk or posedge reset)
    begin
      if(reset)
        q <= 0;
-        else
-          begin
-            for(i=0; i<4; i=i+1)
-               begin
-                 q[]
+         else
+            begin
+               q <= {serial_in, q[n-1:1]};
+               //for right shift, q[0] eliminated after copying to serial_out
+               serial_out <= q[0];
+            end
+   end
+endmodule 
