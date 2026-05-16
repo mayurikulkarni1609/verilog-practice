4b38ec02829649dbc2169a18bd3cd2cf78cf050c
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-17 00:55:13 +0530

    verilog module for parallel_in serial_out shift register

--- modules_sequential/piso.v
+++ modules_sequential/piso.v
@@ -1,6 +1,6 @@
 module piso #(parameter n = 4)
 (
-    input clk, reset, shift, load,
+    input clk, reset, load,
     input [n-1:0]parallel_in,
     output reg [n-1:0]q,
     output reg serial_out
@@ -8,19 +8,16 @@ module piso #(parameter n = 4)
 always@(posedge clk or posedge reset)
    begin
      if(reset)
-     q <= 0;
-     else if(load)
-     q  <= parallel_in;
-     else
        begin
-          genvar i;
-          generate
-            for(i=n; i>0; i=i-1)
-              begin
-                serial_out <= q[0];
-                q >> 1;
-              end
-          endgenerate
+         q <= 0;
+         serial_out <= 0;
        end
+          else if(load)
+            q  <= parallel_in;
+          else
+            begin
+               serial_out <= q[0];
+               q <= q>>1;
+            end
    end
 endmodule
