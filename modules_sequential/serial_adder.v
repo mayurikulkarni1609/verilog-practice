62ba0a3c67ffe199d0c3e09058b772d5ec9ab701
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-25 23:47:26 +0530

    Update serial_adder.v

--- modules_sequential/serial_adder.v
+++ modules_sequential/serial_adder.v
@@ -1,1 +1,12 @@
 module serial_adder
+(
+    input clk, reset, start, 
+    input [3:0]a, b,
+    output reg [3:0]result,
+    output reg done
+);
+
+  reg current_state, next_state;
+  reg [3:0]count;
+  wire shift;
+  wire x, y, 
