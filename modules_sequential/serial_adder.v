b8fb103920a5fef72f21730761453895aa1557ff
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-26 01:38:41 +0530

    Update serial_adder.v

--- modules_sequential/serial_adder.v
+++ modules_sequential/serial_adder.v
@@ -6,7 +6,34 @@ module serial_adder
     output reg done
 );
 
+
+  reg [3:0]acc, addend;
   reg [1:0]count;
   wire shift;
-  wire x, y, sum, c_out;
+  wire sum, c_out;
   reg carry; //this is the D flip-flop
+  reg x, y;
+
+
+    always@(posedge clk or posedge reset)
+       begin
+        acc <= a;
+        addend <= b;
+         if(~reset)
+           count <= 0;
+             else if(~start)
+               count <= 0;
+                 else
+                   begin
+                    acc <= {sum, acc[3:1]};
+                    addend <= {0, addend[3:1]};
+                    x <= acc[0];
+                    y <= addend[0];
+
+                     
+                   end
+
+
+               
+       end
+  
